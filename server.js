7490e4d99436e30ad1ba3b421f613d400f6801bf
jaiprakash <jai.prakash@qodequay.com>
2024-01-17 11:57:31 +0530

    update and delete product route made

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const PORT = 3000;
 const uri = 'mongodb+srv://Jaiprakash:tj952920@testcluster.vbndu4a.mongodb.net/?retryWrites=true&w=majority'
 
 app.use(express.json())
+// urlencoded is use to allow the form element updation 
+app.use(express.urlencoded({extended: false}))
 
 // routes
 app.get('/',(req, res) =>{
     res.send('Hello World');
 })
+
 
 app.get("/products", async(req, res) => {
     try {
@@ -39,6 +42,35 @@ app.post('/product', async(req, res) => {
     } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        // if the product is not available with that particular then return 404 message
+        // otherwise update the product with relative to id
+        if(!product){
+            return res.status(404).json({message: `cannot find any product by ${id}`})
+        }
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.delete('/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id)
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with this ${id}`})
+        }
+        res.status(200).json({message : `product is deleted buy this id ${id}`})
+    } catch (error) {
+        res.status(500).json({message : error.message})
     }
 })
 
